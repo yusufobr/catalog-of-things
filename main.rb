6043843ea121ece9d5d566116b78c8d1be03ff50
clin2on3mun <munanaclinton@gmail.com>
2023-09-11 17:05:42 +0200

    Add main.rb and user option to choose

---
+++ main.rb
@@ -0,0 +1,14 @@
+puts '1. Add Book'
+puts '2. Add Game'
+puts '3. Add music Album'
+puts '4. Add movie'
+puts '5. List all books'
+puts '6. List all music albums'
+puts '7. List all movies'
+puts '8. List of games'
+puts '9. List all genres'
+puts '10. List all labels'
+puts '11. List all authors'
+puts '12. List all sources'
+
+gets.chomp
