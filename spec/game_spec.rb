9ac0d99bc14e1bcac3ac209b4587af8dff8f1dc8
clin2on3mun <munanaclinton@gmail.com>
2023-09-12 14:37:03 +0200

    Add test for author and game classes

---
+++ spec/game_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../game'
+
+describe Game do
+  context 'Test for Game object' do
+    let(:game) { Game.new('2002-02-01', 3, '2020-08-01') }
+    it 'check for published date' do
+      expect(game.publish_date).to eq('2002-02-01')
+    end
+    it 'check for can_be_archived' do
+      expect(game.can_be_archived?).to eq(true)
+    end
+    it 'check for last_date state' do
+      expect(game.last_played_at).to eq('2020-08-01')
+    end
+  end
+end
