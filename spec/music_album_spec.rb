52ab43478cd530cbc7a8eb1413614d7bb4ae1d49
tonnytech <tonnytei4@gmail.com>
2023-09-13 11:39:30 +0300

    add tests for the music classes

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,18 +1,16 @@
-require_relative '../music_album'
-require_relative '../item'
+require_relative '../classes/music/music_album'
+require_relative '../classes/item'
 
 
 describe MusicAlbum do
-    it "#add album item" do
+  it '#add album item' do
     date = '2020-08-08'
     album = MusicAlbum.new(date)
     expect(album.publish_date).to eq(date)
-    end
+  end
 
-    it "return true while on spotify and earlire than 10yrs" do
-        test_item = MusicAlbum.new("2020-08-08", on_spotify: true)
-        expect(test_item.can_be_archived?).to eq(false)
-    end
+  it 'return true while on spotify and earlire than 10yrs' do
+    test_item = MusicAlbum.new('2020-08-08', on_spotify: true)
+    expect(test_item.can_be_archived?).to eq(false)
+  end
 end
-
-
