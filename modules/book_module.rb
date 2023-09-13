96e6c632b21f8855ceaaae1f48a4df53813c68f3
yusufobr <yousarsif80@gmail.com>
2023-09-13 14:16:13 +0100

    create book module

---
+++ modules/book_module.rb
@@ -0,0 +1,23 @@
+require_relative '../classes/books/book'
+
+module BookModule
+  def create_book(publisher, cover_state, publish_date)
+    book = Book.new(publisher, cover_state, publish_date)
+    @books << book
+    save_book_to_file(@books)
+  end
+
+  def list_books
+    if @books.empty?
+      puts 'There are no books'
+    else
+      @books.each_with_index do |book, i|
+        puts "#{i + 1}) Publisher: #{book.publisher}, Cover state: #{book.cover_state}, Published: #{book.publish_date}"
+      end
+    end
+  end
+
+  def save_book_to_file(books)
+    @book_data.save_books(books)
+  end
+end
