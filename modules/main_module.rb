b03e52ab982d33c8faed9bcd24fbce5fa694b8f8
yusufobr <yousarsif80@gmail.com>
2023-09-13 18:00:54 +0100

    Add add book method in main_module file

--- modules/main_module.rb
+++ modules/main_module.rb
@@ -9,4 +9,15 @@ module MainModule
     publish_date_input = gets.chomp
     app.create_game(multiplayer_input, last_played_at_date, publish_date_input)
   end
+
+  def add_book(app)
+    puts "\n"
+    puts 'Who is the publisher [Enter Publisher Name]:'
+    publisher = gets.chomp
+    puts 'What Is the cover\'s state like? [Enter good or bad]:'
+    cover_state = gets.chomp
+    puts 'When was the book published [Enter date format year/month/date]:'
+    publish_date = gets.chomp
+    app.create_book(publisher, cover_state, publish_date)
+  end
 end
