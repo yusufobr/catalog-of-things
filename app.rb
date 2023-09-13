664533a3375298cca5ce285bca129f994b32ae2d
yusufobr <yousarsif80@gmail.com>
2023-09-13 14:16:43 +0100

    add book and label module

--- app.rb
+++ app.rb
@@ -2,19 +2,29 @@ require_relative 'modules/game_module'
 require_relative 'modules/author_module'
 require_relative 'modules/genre_module'
 require_relative 'modules/music_album_module'
+require_relative 'modules/book_module'
+require_relative 'modules/label_module'
 
 class App
-  attr_accessor :authors, :games, :genre, :albums
+  attr_accessor :authors, :games, :genre, :albums, :books, :labels
 
   def initialize
     @authors = []
     @games = []
     @genres = []
     @albums = []
+    @books = []
+    @labels = []
   end
 
   include AuthorModule
   include GameModule
   include GenreModule
   include MusicAlbumModule
+  include BookModule
+  include LabelModule
+
 end
+
+app = App.new
+app.list_labels
