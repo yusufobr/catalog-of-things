cc3aa5df2a0d2bcca1aa1da515adc118511a3c1b
clin2on3mun <munanaclinton@gmail.com>
2023-09-13 18:18:35 +0200

    Add changes to games_data and author_data.rb

--- storage/author_data.rb
+++ storage/author_data.rb
@@ -1,4 +1,5 @@
 require_relative '../classes/games/author'
+require 'json'
 
 class AuthorManager
   DATA_FOLDER = 'json/'.freeze
@@ -6,7 +7,7 @@ class AuthorManager
   def load_authors
     return [] unless File.exist?("#{DATA_FOLDER}authors.json")
 
-    author_data = JSON.parse(File.read("#{DATA_FOLDER}authors.json"))
+    author_data = JSON.parse(File.read("#{DATA_FOLDER}authors.json")) || []
     authors = []
     author_data.map { |data| authors << Author.new(data.first_name, data.last_name) }
     authors
--- storage/games_data.rb
+++ storage/games_data.rb
@@ -1,10 +1,11 @@
 require_relative '../classes/games/game'
-
+require 'json'
 class GameData
   DATA_FOLDER = 'json/'.freeze
   def save_games_data(games)
+
     File.open("#{DATA_FOLDER}games.json", 'w') do |file|
-      data_of_games = [
+      data_of_games = 
         games.map do |game|
           {
             'multiplayer' => game.multiplayer,
@@ -13,7 +14,7 @@ class GameData
             'id' => game.id
           }
         end
-      ]
+      
       file.write(JSON.pretty_generate(data_of_games))
     end
   end
@@ -21,7 +22,7 @@ class GameData
   def load_games_data
     return [] unless File.exist?("#{DATA_FOLDER}games.json")
 
-    data_of_games = JSON.parse(File.read("#{DATA_FOLDER}games.json"))
+    data_of_games = JSON.parse(File.read("#{DATA_FOLDER}games.json")) || []
     games = []
     data_of_games.map do |game|
       games << Game.new(game.multiplayer, game.last_played_at, game.publish_date)
