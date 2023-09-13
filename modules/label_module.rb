6fdb4a39cb0e7eb248cd9d3fdbaff11cd3ecae3d
yusufobr <yousarsif80@gmail.com>
2023-09-13 14:16:27 +0100

    create label module

---
+++ modules/label_module.rb
@@ -0,0 +1,13 @@
+require_relative '../classes/books/label'
+
+module LabelModule
+  def list_labels
+    if @labels.empty?
+      puts 'There are no labels'
+    else
+      @labels.each_with_index do |label, i|
+        puts "Number: #{i + 1}, title: #{label.title}, color: #{label.color}"
+      end
+    end
+  end
+end
